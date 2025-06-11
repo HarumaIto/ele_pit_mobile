70e0cc36e562562423cfa40a8ed95f9df827ca2b
HarumaIto <itoharm@gmail.com>
2025-06-12 00:57:49 +0900

    refactor: 厳密な処理をRepositoryとして切り出し、依存性注入を行うことで、テストを容易にする

--- lib/provider/view_model/view_model_auth.dart
+++ lib/provider/view_model/view_model_auth.dart
@@ -1,5 +1,5 @@
 import 'package:ele_pit/model/view_model/state_auth.dart';
-import 'package:ele_pit/provider/global/provider_firebase_auth.dart';
+import 'package:ele_pit/repository/repository_google_signin.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,27 +8,23 @@ part 'view_model_auth.g.dart';
 
 @riverpod
 class AuthViewModel extends _$AuthViewModel {
+  late final GoogleSignInRepository _googleSignInRepository;
+
   @override
   AuthState build() {
+    _googleSignInRepository = GoogleSignInRepository(
+      googleSignIn: GoogleSignIn(),
+      firebaseAuth: FirebaseAuth.instance,
+    );
+
     return const AuthState();
   }
 
   Future<void> signInWithGoogle() async {
     state = state.copyWith(isLoading: true);
     try {
-      final googleUser = await GoogleSignIn().signIn();
-      if (googleUser == null) {
-        state = state.copyWith(isLoading: false, error: 'Google認証がキャンセルされました');
-        return;
-      }
-      final googleAuth = await googleUser.authentication;
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      final instance = ref.read(firebaseAuthProvider);
-      await instance.signInWithCredential(credential);
-      state = state.copyWith(isLoading: false, error: '');  
+      await _googleSignInRepository.signIn();
+      state = state.copyWith(isLoading: false, error: '');
     } catch (e) {
       state = state.copyWith(isLoading: false, error: 'Google認証に失敗しました: $e');
     }
